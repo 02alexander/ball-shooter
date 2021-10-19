eebcabc1fe2dc8c9e80b2094c713c8fb79096e68
Alexander Berntsson <alexande.berntsson@gmail.com>
2021-10-19 23:39:49 +0200

    controller follows ball then shoots when still

--- shooter/controller/controller.py
+++ shooter/controller/controller.py
@@ -14,28 +14,40 @@ from shooter.utils import draw_rectangle, kmeans, abs_diff, highlight_pixels
 # -parenb -parodd -cmspar cs8 hupcl -cstopb cread clocal -crtscts -ignbrk -brkint -ignpar -parmrk -inpck -istrip -inlcr -igncr icrnl ixon -ixoff -iuclc -ixany -imaxbel -iutf8 opost olcuc ocrnl onlcr -onocr -onlret -ofill -ofdel nl0 cr0 tab0 bs0 vt0 ff0 isig icanon iexten echo echoe echok -echonl -noflsh -xcase -tostop -echoprt echoctl echoke -flusho -extproc
 
 class Controller:
-    def __init__(self, cammodel, shooter_pos, queuelen=100, projectile_velocity=3.34, fire_delay=0.36):
+    def __init__(self, cammodel, shooter_pos, queuelen=30, projectile_velocity=3.34, fire_delay=0.36):
         self.fd = os.open("/dev/ttyACM0", os.O_RDWR)
         if self.fd == -1:
             self.fd = os.open("/dev/ttyACM1", os.O_RDWR)
         if self.fd == -1:
             raise NameError("Error opening terminal device")
 
-        #attr = termios.tcgetattr(self.fd)
-        #attr[1] = attr[1] & ~(termios.OPOST | termios.ONLCR | termios.CBAUD)
+        attr = termios.tcgetattr(self.fd)
+        attr[1] = attr[1] & ~(termios.ONOCR | termios.ONLCR | termios.OCRNL | termios.OLCUC)
         #attr[1] |= termios.B9600
-        #termios.tcsetattr(self.fd, termios.TCSAFLUSH, attr)
+        termios.tcsetattr(self.fd, termios.TCSAFLUSH, attr)
 
         self.file = os.fdopen(self.fd, "w")
         self.time_last_turned = time.time()
 
         self.cammodel = cammodel
+        
+        # indexed by id
+        self.aruco_markers_img_cord = [
+            np.reshape(cammodel.irl_cord_to_img_point(np.transpose(cammodel.aruco_tvecs[np.where(cammodel.aruco_ids==i)[0][0]])), (2)) for i in range(4)
+        ]
+
         self.shooter_pos = shooter_pos
 
         self.previous_positions = deque(maxlen=queuelen) # list of [(position, time)]
         self.projectile_velocity = projectile_velocity
         self.fire_delay = fire_delay
         self.is_loaded = True
+
+    def ignore_rule(self, cord):
+        v = self.aruco_markers_img_cord[0]-self.aruco_markers_img_cord[3]
+        a = np.cross(v, self.aruco_markers_img_cord[2]-self.aruco_markers_img_cord[3])
+        b = np.cross(v, cord-self.aruco_markers_img_cord[3])
+        return a*b < 0
 
     def get_ball_angle(self, ball_img_cord):
         pos = self.cammodel.img_point_to_pos(ball_img_cord)
@@ -48,9 +60,10 @@ class Controller:
     
     def run_iteration(self, bt, window=True):
         frame = bt.raw_last_frame
-        circle = bt.detect_circle()
+        circle = bt.detect_circle(ignore_rule=lambda cord : self.ignore_rule(cord))
         if circle is None:
-            print("no circle detected")
+            #print("no circle detected")
+            pass
         else:
             center, _radius = circle
             center = np.array(center)
@@ -61,24 +74,19 @@ class Controller:
                 draw_rectangle(frame, np.flip(center)*4, 2)
             
             self.turn(angle*180/np.pi)
-            print(angle)
+            #print(angle)
 
             pos = self.cammodel.img_point_to_pos(center*4)
             self.previous_positions.append((pos, time.time()))
         
         if len(self.previous_positions) == self.previous_positions.maxlen and self.is_loaded:
             velocity = self.estimate_velocity()
-            t = 0.8
-            cur_time = time.time()
-            (last_pos, last_time) = self.previous_positions[-1]
-            pos_after_t = last_pos+velocity*(t+cur_time-last_time)
-            time_required = np.linalg.norm(pos_after_t-self.shooter_pos)/self.projectile_velocity+self.fire_delay
-            angle = self.get_ball_angle(pos_after_t)
-            self.turn(angle*180/np.pi)
-            time.sleep(t-time_required)
-            print("FIRE!")
-            self.shoot()
-            self.is_loaded = False
+            
+            if np.linalg.norm(velocity) < 0.1:
+                print("FIRE!")
+                self.shoot()
+                self.is_loaded = False
+            
 
 
         if window is not None:
@@ -88,7 +96,6 @@ class Controller:
                 cord = self.cammodel.irl_cord_to_img_point(self.cammodel.origo)
                 draw_rectangle(frame, cord, 5)
             cv2.imshow(window, frame)
-
 
     def estimate_velocity(self):
         if len(self.previous_positions) <= 2:
@@ -106,7 +113,7 @@ class Controller:
         if start - self.time_last_turned > 0.001:
             self.file.write(f'{angle:.4}'+'\n')
             end = time.time()
-            print("time to send message ", (end-start))
+            #print("time to send message ", (end-start))
             self.time_last_turned = end
 
 def main():
